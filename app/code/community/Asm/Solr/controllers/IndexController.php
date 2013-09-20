b404b0acd1f4568c5bf19c02aa20022c5f1d51b4
Ingo Renner <ingo@infielddesign.com>
2013-09-20 11:58:06 -0700

    Add frontend controller

---
+++ app/code/community/Asm/Solr/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+class Asm_Solr_IndexController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+
+
+	    echo 'lol';
+    }
+
+}
