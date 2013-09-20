47876256bd48000aad024ae83f72fed2d021fe18
Ingo Renner <ingo@infielddesign.com>
2013-09-20 11:54:34 -0700

    Add data helper

---
+++ app/code/community/Asm/Solr/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Solr data helper
+ *
+ * @category Asm
+ * @package Asm_Solr
+ * @author Ingo Renner <ingo@infielddesign.com>
+ */
+class Asm_Solr_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
