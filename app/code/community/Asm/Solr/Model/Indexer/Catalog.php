58f59658e2b4f06325b78603fa529cd01538a7d8
Ingo Renner <ingo@infielddesign.com>
2013-10-18 12:08:37 -0700

    Add mass indexer

---
+++ app/code/community/Asm/Solr/Model/Indexer/Catalog.php
@@ -0,0 +1,77 @@
+<?php
+
+class Asm_Solr_Model_Indexer_Catalog extends Mage_Index_Model_Indexer_Abstract
+{
+
+	/**
+	 * Data key for matching result to be saved in
+	 */
+	const EVENT_MATCH_RESULT_KEY = 'solr_catalog_match_result';
+
+
+	protected $_matchedEntities = array(
+		Mage_Catalog_Model_Product::ENTITY => array(
+			Mage_Index_Model_Event::TYPE_SAVE,
+			Mage_Index_Model_Event::TYPE_MASS_ACTION,
+			Mage_Index_Model_Event::TYPE_REINDEX
+		),
+	);
+
+
+	protected function _construct()
+	{
+		$this->_init('solr/indexer_catalog');
+	}
+
+	/**
+	 * Get Indexer name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Mage::helper('solr')->__('Solr Catalog Search Index');
+	}
+
+	/**
+	 * Get Indexer description
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return Mage::helper('solr')->__('Rebuild Solr catalog search index');
+	}
+
+	/**
+	 * Register indexer required data inside event object
+	 *
+	 * @param Mage_Index_Model_Event $event
+	 */
+	protected function _registerEvent(Mage_Index_Model_Event $event)
+	{
+		// TODO: Implement _registerEvent() method.
+	}
+
+	protected function registerCatalogProductEvent(Mage_Index_Model_Event $event)
+	{
+		// TODO implement registerCatalogProductEvent() method.
+	}
+
+	/**
+	 * Process event based on event state data
+	 *
+	 * @param Mage_Index_Model_Event $event
+	 */
+	protected function _processEvent(Mage_Index_Model_Event $event)
+	{
+		// TODO: Implement _processEvent() method.
+	}
+
+	public function reindexAll()
+	{
+		$resource = $this->getResource();
+		/** @var Asm_Solr_Model_Resource_Indexer_Catalog $resource */
+		$resource->rebuildIndex();
+	}
+}
