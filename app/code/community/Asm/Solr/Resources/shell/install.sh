47c5aa5bb1cf1ce587bfb093da5ffca5cde869df
Ingo Renner <ingo@infielddesign.com>
2013-10-23 13:57:48 -0700

    Improve installer messages and order of checks

--- app/code/community/Asm/Solr/Resources/shell/install.sh
+++ app/code/community/Asm/Solr/Resources/shell/install.sh
@@ -126,55 +126,66 @@ cecho "Checking requirements." $green
 PASSALLCHECKS=1
 
 # Make sure only root can run this script
+echo -n "Running as root? "
 if [[ $EUID -ne 0 ]]
-then
-	cecho "This script must be run as root." $red
-	exit 1
-fi
-
-# make sure Java is installed
-java -version > /dev/null 2>&1
-CHECK=$?
-if [ $CHECK -ne "0" ]
-then
-	cecho "ERROR couldn't find Java (Oracle Java is recommended)." $red
-	PASSALLCHECKS=0
+	then
+		cecho "ERROR This script must be run as root." $red
+		PASSALLCHECKS=0
+	else cecho "passed" $green
 fi
 
 # wget installed?
+echo -n "Looking for wget: "
 wget --version > /dev/null 2>&1
+CHECK=$?
+if [ $CHECK -ne "0" ]
+	then
+		cecho "ERROR couldn't find wget." $red
+		PASSALLCHECKS=0
+	else cecho "passed" $green
+fi
+
 # test if release branch exists, if so we'll download from there
 wget --no-check-certificate -q -O /dev/null http://www.infielddesign.com/fileadmin/asm/$GITBRANCH_PATH/
 BRANCH_TEST_RETURN=$?
 
+# make sure Java is installed
+echo -n "Looking for Java: "
+java -version > /dev/null 2>&1
 CHECK=$?
 if [ $CHECK -ne "0" ]
-then
-	cecho "ERROR couldn't find wget." $red
-	PASSALLCHECKS=0
+	then
+		cecho "ERROR couldn't find Java (Oracle Java is recommended)." $red
+		PASSALLCHECKS=0
+	else cecho "passed" $green
 fi
 
 # Can we connect to the Internet?
+echo -n "Trying to reach Apache download mirror: "
 ping -c 1 apache.osuosl.org > /dev/null 2>&1
 CHECK=$?
 if [ $CHECK -ne "0" ]
-then
-	cecho "ERROR couldn't ping Apache download mirror, try again using wget" $yellow
-	wget -q -O /dev/null http://apache.osuosl.org
-	if [ $? -ne "0" ]
 	then
-		cecho "ERROR also couldn't wget Apache download mirror at Oregon State University Open Source Lab - OSUOSL" $red
-		PASSALLCHECKS=0
-	fi
+		cecho "WARNING Couldn't ping Apache download mirror. Relax, will try again using wget." $yellow
+		wget -q -O /dev/null http://apache.osuosl.org
+		if [ $? -ne "0" ]
+			then
+				cecho "ERROR Also couldn't reach the Apache download mirror at Oregon State University Open Source Lab - OSUOSL using wget. Please check your internet connection." $red
+				PASSALLCHECKS=0
+			else cecho "passed" $green
+		fi
+	else cecho "passed" $green
 fi
 
 # Do we have unzip installed
+echo -n "Looking for unzip: "
 unzip -v > /dev/null 2>&1
 CHECK=$?
 if [ $CHECK -ne "0" ]
-then
-	cecho "ERROR: couldn't find unzip." $red
-	PASSALLCHECKS=0
+	then
+		cecho "ERROR: couldn't find unzip." $red
+		PASSALLCHECKS=0
+	else cecho "passed" $green
 fi
 
 # Check if solr scheme files etc. for specified languages are available
@@ -183,19 +194,19 @@ do
 	echo -n "Checking availability of language \"$LANGUAGE\": "
 	wgetresource solr/magentocores/conf/"$LANGUAGE"/schema.xml justcheck
 	if [ $? -ne 0 ]
-	then
-		cecho "ERROR: Could not find Solr configuration files for language \"$LANGUAGE\"" $red
-		exit 1
-	else cecho "passed" $green
+		then
+			cecho "ERROR: Could not find Solr configuration files for language \"$LANGUAGE\"" $red
+			PASSALLCHECKS=0
+		else cecho "passed" $green
 	fi
 done
 
 if [ $PASSALLCHECKS -eq "0" ]
-then
-	cecho "Please install missing requirements or fix any other errors listed above and try again." $red
-	exit 1
-else
-	cecho "All requirements met. Hold tight, installing Solr!" $green
+	then
+		cecho "Please install missing requirements or fix any other errors listed above and try again." $red
+		exit 1
+	else
+		cecho "All requirements met. Hold tight, now installing Solr!" $green
 fi
 
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
