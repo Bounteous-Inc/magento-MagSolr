8e4bc2122e52595ba430077be4adf14e97fa1ef9
Ingo Renner <ingo@infielddesign.com>
2013-12-03 12:32:26 -0800

    clean up code style

--- app/code/community/Asm/Solr/Helper/Data.php
+++ app/code/community/Asm/Solr/Helper/Data.php
@@ -39,7 +39,8 @@ class Asm_Solr_Helper_Data extends Mage_Core_Helper_Abstract
 	 * @param integer $productId Product ID
 	 * @return string The document id for that product
 	 */
-	public function getProductDocumentId($productId) {
+	public function getProductDocumentId($productId)
+	{
 		$baseUrl  = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
 		$host     = parse_url($baseUrl, PHP_URL_HOST);
 		$siteHash = $this->getSiteHashForDomain($host);
@@ -55,7 +56,8 @@ class Asm_Solr_Helper_Data extends Mage_Core_Helper_Abstract
 	 * @param string $domain Domain to calculate the site hash for.
 	 * @return string site hash for $domain
 	 */
-	public function getSiteHashForDomain($domain) {
+	public function getSiteHashForDomain($domain)
+	{
 		$encryptionKey = Mage::getStoreConfig('global/crypt/key');
 
 		$siteHash = sha1(
