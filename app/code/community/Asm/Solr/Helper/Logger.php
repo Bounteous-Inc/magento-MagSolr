0b99b0ebcdb8671dec7f096289b71337df379258
Ingo Renner <ingo@infielddesign.com>
2013-12-05 17:19:58 -0800

    Add a logging helper class

---
+++ app/code/community/Asm/Solr/Helper/Logger.php
@@ -0,0 +1,59 @@
+<?php
+
+class Asm_Solr_Helper_Logger
+{
+	protected $logFileName = 'solr.log';
+
+
+	public function log($message, $level, $data = null)
+	{
+		if (!is_null($data)) {
+			$message .= ' ' . json_encode($data);
+		}
+
+		Mage::log($message, $level, 'solr.log');
+	}
+
+	public function emergency($message, $data = null)
+	{
+		$this->log($message, Zend_Log::EMERG, $data);
+	}
+
+	public function alert($message, $data = null)
+	{
+		$this->log($message, Zend_Log::ALERT, $data);
+	}
+
+	public function critical($message, $data = null)
+	{
+		$this->log($message, Zend_Log::CRIT, $data);
+	}
+
+	public function error($message, $data = null)
+	{
+		$this->log($message, Zend_Log::ERR, $data);
+	}
+
+	public function warning($message, $data = null)
+	{
+		$this->log($message, Zend_Log::WARN, $data);
+	}
+
+	public function notice($message, $data = null)
+	{
+		$this->log($message, Zend_Log::NOTICE, $data);
+	}
+
+	public function information($message, $data = null)
+	{
+		$this->log($message, Zend_Log::INFO, $data);
+	}
+
+	public function debug($message, $data = null)
+	{
+		$this->log($message, Zend_Log::DEBUG, $data);
+	}
+}
+
+
+?>
