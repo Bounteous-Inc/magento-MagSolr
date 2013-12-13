ba1a623eb938a403ea06a9de76e6cd5f954c4c72
Ingo Renner <ingo@infielddesign.com>
2013-12-13 12:32:33 -0800

    Add methods to get Solr connections by site, store, or all connections

--- app/code/community/Asm/Solr/Helper/ConnectionManager.php
+++ app/code/community/Asm/Solr/Helper/ConnectionManager.php
@@ -24,11 +24,66 @@ class Asm_Solr_Helper_ConnectionManager
 		return $connection;
 	}
 
-	public function getConnectionByStore($storeId)
+	/**
+	 * @param string|int $store Store code or store ID
+	 * @return Asm_Solr_Model_Solr_Connection
+	 */
+	public function getConnectionByStore($store)
 	{
+		$storeCode = $store;
 
+		if (is_numeric($store)) {
+			$storeCode = Mage::getModel('core/store')
+				->getCollection()
+				->addIdFilter($store)
+				->getFirstItem()
+				->getCode();
+		}
+
+		$solrConnectionParameters = Mage::getConfig()->getNode('solr/connection', 'stores', $storeCode);
+		$connection = Mage::getModel('solr/solr_connection', $solrConnectionParameters);
+
+		return $connection;
 	}
 
+	public function getConnectionsBySite($site)
+	{
+		$connections = array();
 
+		if (is_string($site) && !is_numeric($site)) {
+			// website code
+			$collection = Mage::getModel('core/website')->getCollection()->addFieldToFilter('code', $site);
+			$website    = $collection->getFirstItem();
+		} else {
+			// website id
+			$website = Mage::getModel('core/website')->getCollection()->addIdFilter($site)->getFirstItem();
+		}
+
+		$stores = $website->getStoreCollection();
+		foreach ($stores as $store) {
+			/** @var Mage_Core_Model_Store $store */
+			$storeConnection = $this->getConnectionByStore($store->getId());
+			$connections[] = $storeConnection;
+		}
+
+		return $connections;
+	}
+
+	public function getAllConnections()
+	{
+		$connections = array();
+		$websites = Mage::getModel('core/website')->getCollection()->addFieldToFilter('website_id', array("neq" => '0'));
+
+		foreach ($websites as $website) {
+			/** @var Mage_Core_Model_Website $website */
+			$id = $website->getId();
+			$websiteConnections = $this->getConnectionsBySite($website->getId());
+			$connections = array_merge($connections, $websiteConnections);
+		}
+
+		return $connections;
+	}
 
 }
+
+?>
