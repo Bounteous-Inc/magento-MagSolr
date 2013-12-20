f0ae2733a8937bf224a08b9e43d9d786fdbb0f2e
Ingo Renner <ingo@infielddesign.com>
2013-12-19 16:12:09 -0800

    whitespace cleanup

--- app/code/community/Asm/Solr/controllers/IndexController.php
+++ app/code/community/Asm/Solr/controllers/IndexController.php
@@ -2,14 +2,12 @@
 
 class Asm_Solr_IndexController extends Mage_Core_Controller_Front_Action
 {
-    /**
-     * Index action
-     */
-    public function indexAction()
-    {
-
-
-	    echo 'lol';
-    }
+	/**
+	 * Index action
+	 */
+	public function indexAction()
+	{
+		echo 'solr';
+	}
 
 }
