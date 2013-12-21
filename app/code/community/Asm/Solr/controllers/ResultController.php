2eaf8a2ffe3fb0bb905171aea72c956221796f67
Ingo Renner <ingo@infielddesign.com>
2013-12-20 17:55:33 -0800

    Add offset and limit to query

--- app/code/community/Asm/Solr/controllers/ResultController.php
+++ app/code/community/Asm/Solr/controllers/ResultController.php
@@ -4,23 +4,25 @@ class Asm_Solr_ResultController extends Mage_Core_Controller_Front_Action {
 
 	public function indexAction()
 	{
+		$this->loadLayout();
+		$layout = $this->getLayout();
 
-		$connection = Mage::helper('solr/connectionManager')->getConnection();
-
-		/** @var $query Asm_Solr_Model_Solr_Query */
+		/** @var Asm_Solr_Model_Solr_Query $query */
 		$query = Mage::getModel('solr/solr_query', array(
 			'keywords' => $this->getRequest()->getParam('q')
 		));
 
+		$listToolbar = $layout->getBlock('product_list_toolbar');
+		$limit  = $listToolbar->getLimit();
+		$offset = $limit * ($listToolbar->getCurrentPage() - 1);
 
 
 		$search   = Mage::getResourceModel('solr/search');
-		$response = $search->search($query);
+		$response = $search->search($query, $offset, $limit);
 
+		$numberOfResults = $response->response->numFound;
 		$resultDocuments = $response->response->docs;
 
-
-		$this->loadLayout();
 
 		/** @var Mage_Catalog_Model_Resource_Product_Collection $collection */
 		$collection = Mage::getModel('catalog/product')->getCollection();
@@ -44,13 +46,9 @@ class Asm_Solr_ResultController extends Mage_Core_Controller_Front_Action {
 		$collection->_setIsLoaded(true);
 		$collection->addAttributeToFilter('entity_id', array('in' => $productIds));
 
-//		$collection = Mage::getResourceModel('catalog/product_collection')
-//			->addAttributeToFilter('entity_id', array('in'=>$ids));
 
-		$layout = $this->getLayout();
 		/** @var Mage_Catalog_Block_Product_List $listBlock */
 		$listBlock = $layout->getBlock('search_result_list');
-
 		$listBlock->setCollection($collection);
 
 		$this->renderLayout();
