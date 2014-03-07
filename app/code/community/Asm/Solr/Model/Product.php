3ea6f91841d9576251268c8895d11cb2eb78d611
Ingo Renner <ingo@infielddesign.com>
2014-03-06 17:47:55 -0800

    improve a comment

--- app/code/community/Asm/Solr/Model/Product.php
+++ app/code/community/Asm/Solr/Model/Product.php
@@ -11,7 +11,7 @@ class Asm_Solr_Model_Product extends Mage_Catalog_Model_Product
 		$this->_init('catalog/product');
 
 		// setting the resource collection to our own
-		// otherwise the same as _init(). However, we're still calling _init()
+		// otherwise it is the same as _init(). However, we're still calling _init()
 		// in case the implementation higher up in the hierarchy changes
 		$this->_setResourceModel('catalog/product', 'solr/product_collection');
 	}
