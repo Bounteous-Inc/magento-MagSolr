058d15b75084b4b04fd883dfade21b34e6472b3f
Ingo Renner <ingo@infielddesign.com>
2014-03-27 15:59:29 -0700

    Add facets template and block

---
+++ app/code/community/Asm/Solr/Block/Facets.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Asm_Solr_Block_Facets extends Mage_Core_Block_Template
+{
+	protected $displayProductCount = null;
+
+	/**
+	 * Whether to show the number of products that will be returned when
+	 * applying a filter.
+	 *
+	 * @return bool
+	 */
+	public function shouldDisplayProductCount()
+	{
+		if ($this->displayProductCount === null) {
+			$this->displayProductCount = Mage::helper('catalog')->shouldDisplayProductCountOnLayer();
+		}
+
+		return $this->displayProductCount;
+	}
+
+	/**
+	 * Gets the facets returned for the current/last query
+	 *
+	 * @return Asm_Solr_Model_Solr_Facet_Facet[]
+	 */
+	public function getFacets() {
+		return Mage::helper('solr')->getResponse()->getFacetFields();
+	}
+}
