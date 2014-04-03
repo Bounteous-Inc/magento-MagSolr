9588b5bf108137a2f6c4415e76d8b9004ab8f059
Ingo Renner <ingo@infielddesign.com>
2014-04-02 17:50:31 -0700

    Add methods to build add/remove filter URLs

--- app/code/community/Asm/Solr/Model/Solr/Facet/FacetOption.php
+++ app/code/community/Asm/Solr/Model/Solr/Facet/FacetOption.php
@@ -10,4 +10,45 @@
 class Asm_Solr_Model_Solr_Facet_FacetOption extends Varien_Object
 {
 
+	protected $unwantedUrlParameters;
+
+
+	public function __construct()
+	{
+		$this->unwantedUrlParameters = array(
+			Mage::getBlockSingleton('page/html_pager')->getPageVarName() => null // reset page browser
+		);
+
+		parent::__construct();
+	}
+
+
+	public function getAddUrl()
+	{
+		$query = array_merge(
+			array($this->getFacet()->getName() => $this->getValue()),
+			$this->unwantedUrlParameters
+		);
+
+		return Mage::getUrl('*/*', array(
+			'_current'     => true,
+			'_use_rewrite' => true,
+			'_query'       => $query
+		));
+	}
+
+	public function getRemoveUrl()
+	{
+		$query = array_merge(
+			array($this->getFacet()->getName() => null),
+			$this->unwantedUrlParameters
+		);
+
+		return Mage::getUrl('*/*', array(
+			'_current' => true,
+			'_use_rewrite' => true,
+			'_query' => $query
+		));
+	}
+
 }
