0ef03d5df3146fb0b1a48617f6364e5eaacc391b
Ingo Renner <ingo@infielddesign.com>
2014-04-04 17:58:48 -0700

    Add a block to render applied filters

---
+++ app/code/community/Asm/Solr/Block/FiltersApplied.php
@@ -0,0 +1,31 @@
+<?php
+
+class Asm_Solr_Block_FiltersApplied extends Mage_Core_Block_Template
+{
+
+	/**
+	 *
+	 *
+	 * @return Asm_Solr_Model_Solr_Facet_FacetOption[]
+	 */
+	public function getAppliedFilters()
+	{
+		$filters  = array();
+		$facets   = Mage::helper('solr')->getResponse()->getFacetFields();
+		/** @var Asm_Solr_Model_Solr_Facet_Facet[] $facets */
+		$urlQuery = Mage::getModel('core/url')->getRequest()->getQuery();
+
+		foreach ($facets as $facet) {
+			$attributeCode = $facet->getAttributeCode();
+			if (array_key_exists($attributeCode, $urlQuery)) {
+				$optionValue = $urlQuery[$attributeCode];
+				$facetOption = $facet->getOption($optionValue);
+
+				$filters[$attributeCode] = $facetOption;
+			}
+		}
+
+		return $filters;
+	}
+
+}
