eb7b47e2abb3f30b0b5341171447bfffef315fa4
Ingo Renner <ingo@infielddesign.com>
2014-04-10 16:33:22 -0600

    Add phpDoc

--- app/code/community/Asm/Solr/Model/Solr/Facet/FacetOption.php
+++ app/code/community/Asm/Solr/Model/Solr/Facet/FacetOption.php
@@ -10,11 +10,22 @@
 class Asm_Solr_Model_Solr_Facet_FacetOption extends Varien_Object
 {
 
+	/**
+	 * URL parameters to remove when adding/removing facet options
+	 *
+	 * @var array
+	 */
 	protected $unwantedUrlParameters;
 
 
+	/**
+	 * Constructor, initializes URL parameters to remove when changing
+	 * facet combinations.
+	 *
+	 */
 	public function __construct()
 	{
+		// Go back to page 1 when adding/removing a facet option
 		$this->unwantedUrlParameters = array(
 			Mage::getBlockSingleton('page/html_pager')->getPageVarName() => null // reset page browser
 		);
@@ -22,7 +33,11 @@ class Asm_Solr_Model_Solr_Facet_FacetOption extends Varien_Object
 		parent::__construct();
 	}
 
-
+	/**
+	 * Gets the URL to add the facet option as a filter to the current query
+	 *
+	 * @return string URL with parameters to add a new filter to the query
+	 */
 	public function getAddUrl()
 	{
 		$query = array_merge(
@@ -37,6 +52,11 @@ class Asm_Solr_Model_Solr_Facet_FacetOption extends Varien_Object
 		));
 	}
 
+	/**
+	 * Gets the current URL with the facet option's filter removed
+	 *
+	 * @return string URL with the facet option's filter parameter removed
+	 */
 	public function getRemoveUrl()
 	{
 		$query = array_merge(
