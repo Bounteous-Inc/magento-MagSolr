550dfc18b6ccf32d9cde3e006a0622e8420e7d8c
Ingo Renner <ingo@infielddesign.com>
2014-05-23 17:30:33 -0700

    Create basic page document

--- app/code/community/Asm/Solr/Model/Resource/Indexer/Cms.php
+++ app/code/community/Asm/Solr/Model/Resource/Indexer/Cms.php
@@ -2,7 +2,6 @@
 
 class Asm_Solr_Model_Resource_Indexer_Cms extends Mage_Core_Model_Resource_Db_Abstract
 {
-
 	/**
 	 * Resource initialization
 	 */
@@ -14,6 +13,7 @@ class Asm_Solr_Model_Resource_Indexer_Cms extends Mage_Core_Model_Resource_Db_Ab
 	 * @param Mage_Index_Model_Event $event
 	 */
 	public function CmsPageSave(Mage_Index_Model_Event $event) {
+		$this->indexPage($event->getData('solr_update_page_id'));
 	}
 
 	/**
@@ -26,7 +26,33 @@ class Asm_Solr_Model_Resource_Indexer_Cms extends Mage_Core_Model_Resource_Db_Ab
 	{
 
 	}
+
+	protected function indexPage($pageId)
 	{
+		$helper  = Mage::helper('solr');
+		$baseUrl = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+		$host    = parse_url($baseUrl, PHP_URL_HOST);
+
+		$page = Mage::getModel('cms/page')->load($pageId);
+
+		$document = new Apache_Solr_Document();
+
+		$document->setField('appKey', 'Asm_Solr');
+		$document->setField('type',   'cms/page');
+
+		$document->setField('id',       $helper->getPageDocumentId($pageId));
+		$document->setField('site',     $host);
+		$document->setField('siteHash', $helper->getSiteHashForDomain($host));
+
+		$document->setField('created', $helper->dateToIso($page->getCreationTime()));
+		$document->setField('changed', $helper->dateToIso($page->getUpdateTime()));
+
+		$document->setField('pageId',  $pageId);
+
+		$document->setField('title',    $page->getTitle());
+		$document->setField('content',  $page->getContent());
+		$document->setField('keywords', $helper->trimExplode(',', $page->getMetaKeyword(), true));
+		$document->setField('url',      Mage::helper('cms/page')->getPageUrl($pageId));
 
 	}
 
