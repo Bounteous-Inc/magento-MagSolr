24b95de57c1a0c9529892151ecfe3c1aa64caffd
Ingo Renner <ingo@infielddesign.com>
2014-05-23 17:13:52 -0700

    Update rebuildIndex() method signature to allow different ways to call it

--- app/code/community/Asm/Solr/Model/Resource/Indexer/Cms.php
+++ app/code/community/Asm/Solr/Model/Resource/Indexer/Cms.php
@@ -19,9 +19,13 @@ class Asm_Solr_Model_Resource_Indexer_Cms extends Mage_Core_Model_Resource_Db_Ab
 	/**
 	 * Rebuild the index for all stores at once or just one specific store.
 	 *
-	 * @param int|null $storeId Store to re-index
+	 * @param int|null $storeId Store to re-index single int store ID to re-index single store, null to re-index all stores
+	 * @param int|array|null $pageIds Page to re-index, single int page ID to re-index a single page, array of page IDs to re-index multiple pages, null to re-index all pages
 	 */
-	public function rebuildIndex($storeId = null)
+	public function rebuildIndex($storeId = null, $pageIds = null)
+	{
+
+	}
 	{
 
 	}
