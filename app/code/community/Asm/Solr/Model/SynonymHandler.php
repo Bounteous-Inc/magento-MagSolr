ade9a92f6abb8ffd963d1c200b2f4fed21ddcab8
Ingo Renner <ingo@infielddesign.com>
2014-06-06 17:58:39 -0700

    Update Solr synonyms when editing search queries in AdminHtml

---
+++ app/code/community/Asm/Solr/Model/SynonymHandler.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2014 Infield Design
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License .
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied .
+ * See the License for the specific language governing permissions and
+ * limitations under the License .
+ */
+
+
+class Asm_Solr_Model_SynonymHandler
+{
+
+
+	public function updateSynonyms($event)
+	{
+		$connection = Mage::helper('solr/connectionManager')->getConnection();
+
+		$query = $event->getDataObject();
+		/* @var $query Mage_CatalogSearch_Model_Query */
+
+		$baseWord    = $query->getData('query_text');
+		$oldSynonyms = $query->getOrigData('synonym_for');
+		$newSynonyms = $query->getData('synonym_for');
+
+		$solrSynonyms = $connection->getSynonyms($baseWord);
+		if (!empty($solrSynonyms) && $newSynonyms != $oldSynonyms) {
+			// since there's no update/edit, simply remove previous mapping
+			// otherwise synonyms only get added, but never removed to/from a base word
+			$connection->deleteSynonym($baseWord);
+		}
+
+		$newSynonyms = Mage::helper('solr')->trimExplode(',', $newSynonyms);
+		$connection->addSynonym($baseWord, $newSynonyms);
+	}
+
+
+}
