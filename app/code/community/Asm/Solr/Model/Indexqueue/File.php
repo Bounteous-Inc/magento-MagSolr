424b8fc213e001cf8a6b4857189b680f9561e7d4
Ingo Renner <ingo@infielddesign.com>
2014-07-11 17:42:19 -0700

    Add an indexqueue_file entity

---
+++ app/code/community/Asm/Solr/Model/Indexqueue/File.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright 2014 Infield Design
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License .
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied .
+ * See the License for the specific language governing permissions and
+ * limitations under the License .
+ */
+
+/**
+ * Model for files to index linked on pages
+ *
+ * @category Asm
+ * @package Asm_Solr
+ * @author Ingo Renner <ingo@infielddesign.com>
+ */
+class Asm_Solr_Model_Indexqueue_File extends Mage_Core_Model_Abstract {
+
+	protected function _construct()
+	{
+		$this->_init('solr/indexqueue_file');
+	}
+
+}
