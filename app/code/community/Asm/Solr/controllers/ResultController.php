bb2918e25b9bbfc13344be189b9b6c6092345aa2
Ian Anderson <ian@infielddesign.com>
2014-08-25 11:30:17 -0700

    Update result controller with product, page, and file actions

--- app/code/community/Asm/Solr/controllers/ResultController.php
+++ app/code/community/Asm/Solr/controllers/ResultController.php
@@ -2,87 +2,82 @@
 
 class Asm_Solr_ResultController extends Mage_Core_Controller_Front_Action {
 
-	public function indexAction()
+    public function testAction()
+    {
+        /** @var Asm_Solr_Model_Result $result */
+        $result = Mage::getModel('solr/result');
+        $result->setKeywords($this->getRequest()->getParam('q'));
+
+        $limit = 20;
+        $offset = 0;
+        $result->load($limit, $offset);
+
+        $products = $result->getProductCollection();
+
+        foreach ($products as $product)
+        {
+            echo $product->getName() . '<br/>';
+        }
+    }
+
+
+    public function productAction()
+    {
+        $this->loadLayout();
+        $layout = $this->getLayout();
+
+        /** @var Asm_Solr_Model_Result $result */
+        $result = Mage::getModel('solr/result');
+        $result->setKeywords($this->getRequest()->getParam('q'));
+
+        $listToolbar = $layout->getBlock('product_list_toolbar');
+        $limit  = $listToolbar->getLimit();
+        $offset = $limit * ($listToolbar->getCurrentPage() - 1);
+
+        $result->load($limit, $offset);
+
+        /** @var Mage_Catalog_Block_Product_List $listBlock */
+        $listBlock = $layout->getBlock('search_result_list');
+        $listBlock->setCollection($result->getProductCollection());
+        Mage::register('solr_result', $result);
+
+        $this->renderLayout();
+    }
+
+    public function pageAction()
+    {
+        $this
+            ->loadLayout()
+            ->renderLayout();
+
+    }
+
+    public function fileAction()
+    {
+        $this
+            ->loadLayout()
+            ->renderLayout();
+    }
+
+    public function indexAction()
 	{
 		$this->loadLayout();
 		$layout = $this->getLayout();
 
-		/** @var Asm_Solr_Model_Solr_Query $query */
-		$query = Mage::helper('solr')->getQuery();
-		$query->setKeywords($this->getRequest()->getParam('q'));
-		$query->addFilter('type', 'catalog/product');
+        /** @var Asm_Solr_Model_Result $result */
+        $result = Mage::getModel('solr/result');
+        $result->setKeywords($this->getRequest()->getParam('q'));
 
-		$listToolbar = $layout->getBlock('product_list_toolbar');
-		$limit  = $listToolbar->getLimit();
-		$offset = $limit * ($listToolbar->getCurrentPage() - 1);
+        $listToolbar = $layout->getBlock('product_list_toolbar');
+        $limit  = $listToolbar->getLimit();
+        $offset = $limit * ($listToolbar->getCurrentPage() - 1);
 
-
-		$search   = Mage::getResourceModel('solr/search');
-		$response = $search->search($query, $offset, $limit);
-		Mage::register('solr/response', $response);
-
-
-		$numberOfResults = $response->getNumberOfResults();
-		/** @var Apache_Solr_Document[] $resultDocuments */
-		$resultDocuments = $response->getDocuments();
-
-		/** @var Asm_Solr_Model_Resource_Product_Collection $collection */
-		$collection = Mage::getModel('solr/product')->getCollection();
-		$collection->setSize($numberOfResults);
-
-		$productIds = array();
-
-		$fieldToAttributeMap = Mage::helper('solr/schema')->getFieldToAttributeMap();
-		// build a reverse map, remove fields that have no atrribute
-		$fieldToAttributeMap = array_filter($fieldToAttributeMap);
-		$attributeToFieldMap = array_flip($fieldToAttributeMap);
-
-		foreach ($resultDocuments as $document)
-		{
-			$product = Mage::getModel('catalog/product');
-
-			$unmappedDocumentFields = $document->getFieldNames();
-
-			// map "core/static" Solr document fields to Magento product attributes
-			foreach ($attributeToFieldMap as $attribute => $field) {
-				$product->setData($attribute, $document->{$field});
-
-				// remove fields that have been mapped, leaves dynamic fields
-				$fieldKey = array_search($field, $unmappedDocumentFields);
-				if ($fieldKey !== false) {
-					unset($unmappedDocumentFields[$fieldKey]);
-				}
-			}
-
-			$dynamicFields = array_diff(
-				$unmappedDocumentFields,
-				array_keys(Mage::helper('solr/schema')->getFieldToAttributeMap())
-			);
-
-			// map Solr dynamic fields to Magento product attributes
-			$dynamicFieldSuffixes = Mage::helper('solr/schema')->getDynamicFieldSuffixes();
-			foreach ($dynamicFields as $dynamicField) {
-				$fieldNameParts = explode('_', $dynamicField);
-
-				// do we have a valid dynamic field? If so, generate attribute name, map to Solr field
-				if (in_array($fieldNameParts[count($fieldNameParts) - 1], $dynamicFieldSuffixes)) {
-					array_pop($fieldNameParts);
-					$attributeName = implode('_', $fieldNameParts);
-					$product->setData($attributeName, $document->{$dynamicField});
-				}
-			}
-
-			$productIds[] = $document->productId;
-
-			$collection->addItem($product);
-		}
-
-		$collection->addAttributeToFilter('entity_id', array('in' => $productIds));
-
+        $result->load($limit, $offset);
 
 		/** @var Mage_Catalog_Block_Product_List $listBlock */
 		$listBlock = $layout->getBlock('search_result_list');
-		$listBlock->setCollection($collection);
+		$listBlock->setCollection($result->getProductCollection());
+        Mage::register('solr_result', $result);
 
 		$this->renderLayout();
 	}
