00b24e2d318cb9a59a8a2cb4bf4f8bd987875935
Ian Anderson <ian@infielddesign.com>
2014-08-28 12:47:55 -0700

    Remove result handler from result controller product action

--- app/code/community/Asm/Solr/controllers/ResultController.php
+++ app/code/community/Asm/Solr/controllers/ResultController.php
@@ -23,25 +23,8 @@ class Asm_Solr_ResultController extends Mage_Core_Controller_Front_Action {
 
     public function productAction()
     {
-        $this->loadLayout();
-        $layout = $this->getLayout();
-
-        /** @var Asm_Solr_Model_Result $result */
-        $result = Mage::getModel('solr/result');
-        $result->setKeywords($this->getRequest()->getParam('q'));
-
-        $listToolbar = $layout->getBlock('product_list_toolbar');
-        $limit  = $listToolbar->getLimit();
-        $offset = $limit * ($listToolbar->getCurrentPage() - 1);
-
-        $result->load($limit, $offset);
-
-        /** @var Mage_Catalog_Block_Product_List $listBlock */
-        $listBlock = $layout->getBlock('search_result_list');
-        $listBlock->setCollection($result->getProductCollection());
-        Mage::register('solr_result', $result);
-
-        $this->renderLayout();
+        $this->loadLayout()
+            ->renderLayout();
     }
 
     public function pageAction()
@@ -61,25 +44,8 @@ class Asm_Solr_ResultController extends Mage_Core_Controller_Front_Action {
 
     public function indexAction()
 	{
-		$this->loadLayout();
-		$layout = $this->getLayout();
 
-        /** @var Asm_Solr_Model_Result $result */
-        $result = Mage::getModel('solr/result');
-        $result->setKeywords($this->getRequest()->getParam('q'));
-
-        $listToolbar = $layout->getBlock('product_list_toolbar');
-        $limit  = $listToolbar->getLimit();
-        $offset = $limit * ($listToolbar->getCurrentPage() - 1);
-
-        $result->load($limit, $offset);
-
-		/** @var Mage_Catalog_Block_Product_List $listBlock */
-		$listBlock = $layout->getBlock('search_result_list');
-		$listBlock->setCollection($result->getProductCollection());
-        Mage::register('solr_result', $result);
-
-		$this->renderLayout();
+        $this->_redirect('*/*/product', array('_query' => $this->getRequest()->getParams()));
 	}
 
 
