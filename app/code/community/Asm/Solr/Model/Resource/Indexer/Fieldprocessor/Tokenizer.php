3a07b8f177829b9090bb795c9cbc3ee748b23686
Ingo Renner <ingo@infielddesign.com>
2014-09-05 12:44:03 -0700

    Add comments/documentation

--- app/code/community/Asm/Solr/Model/Resource/Indexer/Fieldprocessor/Tokenizer.php
+++ app/code/community/Asm/Solr/Model/Resource/Indexer/Fieldprocessor/Tokenizer.php
@@ -1,23 +1,71 @@
 <?php
+/**
+ * Copyright 2014 Infield Design
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License .
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied .
+ * See the License for the specific language governing permissions and
+ * limitations under the License .
+ */
 
 
+/**
+ * A tokenizing field processor.
+ *
+ * Takes the field's value and tokenizes it using the delimiter as configured.
+ * The delimiter is used as configured, there is no trim() applied to it.
+ * Delimiter defaults to , (comma).
+ *
+ * Example:
+ * <fieldMap>
+ *     <magento_foo_field>
+ *         <type>string<type>
+ *         <processor>
+ *             <tokenizer>
+ *                 <delimiter>,</delimiter>
+ *             <tokenizer>
+ *         </processor>
+ *     </magento_foo_field>
+ * </fieldMap>
+ *
+ * @author Ingo Renner <ingo@infielddesign.com>
+ */
 class Asm_Solr_Model_Resource_Indexer_Fieldprocessor_Tokenizer extends Asm_Solr_Model_Resource_Indexer_Fieldprocessor_Abstract {
 
 	protected $delimiter;
 
 	protected $fieldType;
+	/**
+	 * @var string
+	 */
 
 
+	/**
+	 * @inheritdoc
+	 */
 	public function __construct($parameters) {
 		$this->delimiter = (string) Mage::getConfig()->getNode('global/index/fieldMap/' . $parameters['attributeCode'] . '/processor/tokenizer/delimiter');
 
 		parent::__construct($parameters);
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function getFieldName() {
 		return $this->attribute->getAttributeCode() . '_' . $this->fieldType . 'M';
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function getFieldValue() {
 		return explode($this->delimiter, $this->attributeValue);
 	}
